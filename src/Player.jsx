f34849dd36145cad9e73608330a0ea06fe86b5fe
katherinemoore <katherinemoore789@gmail.com>
2023-07-17 19:14:38 -0700

    Created Scoreboard and Player components

--- src/Player.jsx
+++ src/Player.jsx
@@ -1,24 +1,48 @@
 import React from "react";
 import { useState } from "react";
-function Player() {
+
+function Player({ name, handleDelete }) {
+  const [score, setScore] = useState(0);
+
+  function increment() {
+    setScore(score + 1);
+  }
+  
+  function decrement() {
+    setScore(score - 1);
+  }
+
   return (
     <div className="container-fluid border border-dark p-3 m-3">
       <div className="row justify-content-center">
         {/* Render Name here */}
-        <h2 className="text-center"></h2>
+        <h2 className="text-center">{name}</h2>
       </div>
       <div className="row justify-content-center">
-        <p className="text-center fs-3">{/* Render score here */}</p>
+        {/* Render score here */}
+        <p className="text-center fs-3">Score: {score}</p>
       </div>
       <div className="row justify-content-center">
-        <div className="col-md-6">
+        <div className="col-md-6 my-1">
           {/* Add increment event handler */}
-          <button className="btn btn-outline-primary w-100">Add Point +</button>
+          <button 
+            className="btn btn-outline-primary w-100"
+            onClick={increment}>
+              Add Point +
+          </button>
         </div>
-        <div className="col-md-6">
+        <div className="col-md-6 my-1">
           {/* Add decrement event handler */}
-          <button className="btn btn-outline-danger w-100">
+          <button className="btn btn-outline-danger w-100"
+          onClick={decrement}>
             Remove Point -
+          </button>
+        </div>
+      </div>
+      <div className="row justify-content-center m-2">
+        <div className="col-md-6 text-center">
+          <button className="btn btn-danger" onClick={handleDelete}>
+            DELETE PLAYER
           </button>
         </div>
       </div>
--- src/ScoreBoard.jsx
+++ src/ScoreBoard.jsx
@@ -2,8 +2,26 @@ import { useRef, useState } from "react";
 //Bootstrap has already been downloaded for you
 import "bootstrap/dist/css/bootstrap.min.css";
 import Player from "./Player";
+
 function ScoreBoard() {
- 
+const [players, setPlayers] = useState([]);
+const inputRef = useRef();
+
+function addPlayer() {
+  //Pulls input data as player name
+  let playerName = inputRef.current.value || undefined;
+  setPlayers([...players, { name: playerName, id: Date.now() }]);
+  inputRef.current.value = ""; //Clears name input after adding a player
+}
+
+function deletePlayer(id) {
+  setPlayers(
+    players.filter((item) => {
+      return item.id !== id;
+    })
+  );
+}
+
   return (
     <div className="container">
       <div className="row  text-center">
@@ -12,20 +30,22 @@ function ScoreBoard() {
       <div className="row">
         <div className="col-md-4 m-auto">
           <div class="input-group mb-3">
-            {/* Modify input so that it is either connected to a ref or some kind of input state */}
+            {/* Input Player Name */}
             <input
               type="text"
-              class="form-control"
+              ref={inputRef}
+              className="form-control"
               placeholder="New Player Name"
               aria-label="New Player Name"
               aria-describedby="addPlayer"
               required
             />
-            {/* add Add Player event handling to this button */}
+            {/* Add Player button */}
             <button
               class="btn btn-outline-primary"
               type="button"
               id="addPlayer"
+              onClick={addPlayer}
             >
               Add Player
             </button>
@@ -35,13 +55,14 @@ function ScoreBoard() {
       <div className="row m-auto">
         {players.map((player) => {
           return (
-            <div className="col-md-4">
+            <div className="col-lg">
               {/* Make sure to pass the unique id as a key */}
               <Player
                 key={player.id}
                 name={player.name}
                 // Anonymous arrow function that we can hold off activating
                 // until the user clicks a button inside of the Player component
+                handleDelete={() => deletePlayer(player.id)}
               />
             </div>
           );
